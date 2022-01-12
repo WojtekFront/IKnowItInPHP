0346d498868eb84a0ab062416638ce1cec17f1d7
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2022-01-12 20:31:04 +0100

    troszkę więcej zadań

--- reqruitmentTasks/2czyTekstySaTakieSame.php
+++ reqruitmentTasks/2czyTekstySaTakieSame.php
@@ -1,4 +1,6 @@
 <?php
+//nie jest to najszybsza oraz najkrótsza wersja. 
+//postanowiłem oddelegować do funkji zadania, które można później edytować
 $zmienna1 = "ana";
 $zmienna2 = "ana";
 $zmienna3 = "";
@@ -6,23 +8,27 @@ $zmienna4 = "anna";
 $zmienna5 = "Ana";
 
 // czy zmienne istnieją
-function zmiennaIstnieje($zmienna1, $zmienna2)
+function zmiennaIstnieje($z1, $z2)
 {
-    $result;
-    if (!isset($zmienna1) || !isset($zmienna2)) {
-        $result = true;
-    } else ($result = false);
-    return $result;
+    return (!isset($z1) || !isset($z2)) ? true : false;
 }
 
 // czy są pust
 function zmiennaNieJestPusta($z1, $z2)
 {
-    $result = (!is_null($z1) || !is_null($z2)) ? true : false;
+    return (!is_null($z1) || !is_null($z2)) ? true : false;
 }
 
 // czy zmienne są tego samego typu tekstowego
+function zmiennaJestStringiem($z1, $z2)
+{
+    return (!is_string($z1) || !is_string($z2)) ? true : false;
+}
 
 // czy zmienne czy zmienne są tej samej długości
+function jakieMajaDlugosci($z1, $z2)
+{
+    return (strlen($z1) != strlen($z2))?true:false;
+}
 
 // iterowanie dwóch zmiennych i porównywanie
---
+++ reqruitmentTasks/3drzewo.php
@@ -0,0 +1,13 @@
+<?php
+//już czas po świętach ale w domu mam nadal choinkę więc pora wrzucić ją do systemu
+/*
+ 1      #
+        #
+2      ###
+        #
+3     #####
+        #
+4    #######
+        #
+
+*/
